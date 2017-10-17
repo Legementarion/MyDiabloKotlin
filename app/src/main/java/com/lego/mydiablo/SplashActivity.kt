e8a20b502e77cb91fc9531af4f6805f7e17eb225
Legementarion <legementario357442829>
2017-10-18 00:38:28 +0300

    firebase libs installed

--- app/src/main/java/com/lego/mydiablo/SplashActivity.kt
+++ app/src/main/java/com/lego/mydiablo/SplashActivity.kt
@@ -2,11 +2,14 @@ package com.lego.mydiablo
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import com.crashlytics.android.Crashlytics
+import io.fabric.sdk.android.Fabric
 
 class SplashActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
+        Fabric.with(this, Crashlytics())
     }
 }
