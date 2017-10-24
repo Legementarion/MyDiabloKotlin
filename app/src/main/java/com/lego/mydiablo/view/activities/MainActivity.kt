3c44d7ab6eadb9398af0ae88def3761c71bac6d3
Legementarion <legementario357442829>
2017-10-24 22:09:09 +0300

    activity ++

---
+++ app/src/main/java/com/lego/mydiablo/view/activities/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.lego.mydiablo.view.activities
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import com.lego.mydiablo.R
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
