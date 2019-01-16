8b323a351fc52d3e2ebc401ffefcb1bfe3de121a
Lego <dmitry.kovalenko@yalantis.com>
2019-01-16 11:25:59 +0200

    update dependencies, again

--- app/src/main/java/com/lego/mydiablo/logic/rest/BlizzardApi.kt
+++ app/src/main/java/com/lego/mydiablo/logic/rest/BlizzardApi.kt
@@ -2,7 +2,7 @@ package com.lego.mydiablo.logic.rest
 
 import retrofit2.Retrofit
 import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
-import retrofit2.converter.gson.GsonConverterFactory
+import retrofit2.converter.jackson.JacksonConverterFactory
 
 interface BlizzardApi {
 
@@ -12,8 +12,8 @@ interface BlizzardApi {
         fun create(): BlizzardApi {
             val retrofit = Retrofit.Builder()
                     .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
-                    .addConverterFactory(GsonConverterFactory.create())
-                    .baseUrl("https://api.github.com/")
+                    .addConverterFactory(JacksonConverterFactory.create())
+                    .baseUrl("https://api.github.com/") //todo provide real url
                     .build()
 
             return retrofit.create(BlizzardApi::class.java)
