8ca238fda64caddf2108a42900a423614d1d7888
Lego <dmitry.kovalenko@yalantis.com>
2019-01-23 13:59:35 +0200

    mvvm top base screen

--- mainscreen/src/main/java/com/yalantis/coreui/di/screenModule.kt
+++ mainscreen/src/main/java/com/yalantis/coreui/di/screenModule.kt
@@ -3,6 +3,7 @@ package com.yalantis.coreui.di
 import com.yalantis.coreui.flow.details.HeroDetailViewModel
 import com.yalantis.coreui.flow.favorite.FavoriteViewModel
 import com.yalantis.coreui.flow.search.HeroListViewModel
+import com.yalantis.coreui.flow.top.HeroTopViewModel
 import org.koin.android.viewmodel.ext.koin.viewModel
 import org.koin.dsl.module.module
 
@@ -16,4 +17,8 @@ val heroListModule = module {
 
 val heroDetailModule = module {
     viewModel { HeroDetailViewModel() }
+}
+
+val heroTopModule = module {
+    viewModel { HeroTopViewModel() }
 }
---
+++ mainscreen/src/main/java/com/yalantis/coreui/flow/top/HeroTopViewModel.kt
@@ -0,0 +1,6 @@
+package com.yalantis.coreui.flow.top
+
+import com.yalantis.coreui.base.BaseViewModel
+
+class HeroTopViewModel: BaseViewModel() {
+}
