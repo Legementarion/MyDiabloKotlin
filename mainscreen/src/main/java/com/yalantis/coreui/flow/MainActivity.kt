5b43852a4da2e90d4da2a5fe440444cad030513d
Lego <dmitry.kovalenko@yalantis.com>
2019-01-28 11:32:01 +0200

    coroutines android

--- mainscreen/src/main/java/com/yalantis/coreui/flow/MainActivity.kt
+++ mainscreen/src/main/java/com/yalantis/coreui/flow/MainActivity.kt
@@ -1,10 +1,12 @@
 package com.yalantis.coreui.flow
 
 import android.os.Bundle
+import android.support.design.widget.BottomNavigationView
 import android.support.v7.widget.Toolbar
 import com.yalantis.coreui.R
 import com.yalantis.coreui.base.BaseMvvmActivity
 import com.yalantis.coreui.databinding.*
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : BaseMvvmActivity<ActivityMainBinding, MainViewModel>(R.layout.activity_main) {
 
@@ -13,7 +15,24 @@ class MainActivity : BaseMvvmActivity<ActivityMainBinding, MainViewModel>(R.layo
     override val toolbar: Toolbar
         get() = binding.toolbar
 
+    private val mOnNavigationItemSelectedListener = BottomNavigationView.OnNavigationItemSelectedListener { item ->
+        when (item.itemId) {
+            R.id.menu_favorite -> {
+                return@OnNavigationItemSelectedListener true
+            }
+            R.id.menu_top -> {
+                return@OnNavigationItemSelectedListener true
+            }
+            R.id.menu_search -> {
+                return@OnNavigationItemSelectedListener true
+            }
+        }
+        false
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        bottomNavigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener)
     }
 }
